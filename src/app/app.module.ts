78fd162f115a0f28393e190efc4380e733690896
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-27 17:37:19 +0200

    inicialización de componentes + header + footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserComponent } from './components/browser/browser.component';
+import { HistoryComponent } from './components/history/history.component';
+import { AnalysisComponent } from './components/analysis/analysis.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserComponent,
+    HistoryComponent,
+    AnalysisComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
