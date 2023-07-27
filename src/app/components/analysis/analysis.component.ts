fc1a4c89fa88158a199055185a27889bbef117de
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-27 18:52:00 +0200

    componente analisis

--- src/app/components/analysis/analysis.component.ts
+++ src/app/components/analysis/analysis.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-analysis',
   templateUrl: './analysis.component.html',
   styleUrls: ['./analysis.component.scss']
 })
-export class AnalysisComponent {
+export class AnalysisComponent implements OnInit {
+  //-- Datos del analisis de prueba.
+  analysisData: any = {
+    id: 1,
+    url: "http://webdeejemplo.com",
+    title: "Titulo de ejemplo",
+    description: "Ejemplo de descripción",
+    keywords: ["uno", "dos", "tres"],
+    titles: {
+      h1: 2,
+      h2: 3,
+      h3: 5,
+      h4: 9
+    },
+    html5: true,
+    images: 8,
+    createdAt: "2011-10-05T14:48:00.000Z"
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // Convertimos 'titles' a una matriz
+    this.analysisData.titles = Object.entries(this.analysisData.titles).map(([key, value]) => ({key, value}));
+  }
 }
+
