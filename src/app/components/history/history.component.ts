180652f71e2a7c620245f839314e55d3622f6e34
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-27 18:21:29 +0200

    componente historial

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent {
-
+  //-- Datos de prueba
+  items = [
+    { url: 'https://www.example1.com', date: '2023-07-25 10:00:00' },
+    { url: 'https://www.example2.com', date: '2023-07-24 14:00:00' },
+    { url: 'https://www.example3.com', date: '2023-07-23 09:30:00' },
+    { url: 'https://www.example4.com', date: '2023-07-22 16:45:00' },
+    { url: 'https://www.example5.com', date: '2023-07-21 20:00:00' },
+    { url: 'https://www.example7.com', date: '2023-07-21 20:00:00' },
+    { url: 'https://www.example8.com', date: '2023-07-21 20:00:00' },
+    { url: 'https://www.example9.com', date: '2023-07-21 20:00:00' },
+    { url: 'https://www.example10.com', date: '2023-07-21 20:00:00' },
+    { url: 'https://www.example11.com', date: '2023-07-21 20:00:00' },
+  ];
 }
