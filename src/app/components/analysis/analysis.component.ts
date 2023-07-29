04425a070c38b1c2f09804e4a97d97c9f6613a65
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-29 09:04:14 +0200

    corrección maquetación

--- src/app/components/analysis/analysis.component.ts
+++ src/app/components/analysis/analysis.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PostAnalysisResponse } from 'src/app/models/post-analysis-response';
 import { AnalyzerService } from 'src/app/shared/analyzer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-analysis',
@@ -12,14 +12,23 @@ export class AnalysisComponent implements OnInit {
   //-- Variable para transformar el campo titles en un array iterable.
   transformedTitles: any[] = [];
 
-  constructor(public analysisService: AnalyzerService) {}
+  constructor(public analyzerService: AnalyzerService, public router: Router) {}
 
   ngOnInit(): void {
-    this.transformedTitles = this.transformObjectToArray(this.analysisService.analysis.titles);
+    this.transformedTitles = this.transformObjectToArray(this.analyzerService.analysis.titles);
   }
 
   private transformObjectToArray(object: {[key: string]: number}): any[] {
     return Object.entries(object).map(([key, value]) => ({ key, value }));
+  }
+
+  deleteAnalysis(id: number):void{
+    this.analyzerService.deleteAnalysis(id).subscribe((data: any) => {
+      if (data.code == 200) {
+        console.log(data.message);
+      }
+      this.router.navigateByUrl('/informes')
+    })
   }
 }
 
