6981fe6174d732179c1fc681ec256786f03a917b
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-30 09:22:54 +0200

    notificacion de error al usuario añadida

--- src/app/components/browser/browser.component.ts
+++ src/app/components/browser/browser.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { GetAnalysisResponse } from 'src/app/models/get-analysis-response';
 import { DatePipe } from '@angular/common';
 import { PostAnalysisResponse } from 'src/app/models/post-analysis-response';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-browser',
@@ -15,14 +17,23 @@ export class BrowserComponent {
   url: string = '';
   //-- Variable para controlar si se está analizando una URL.
   isAnalyzing = false;
+  //-- Variable para el mensaje si se produce un error.
+  errorMessage: string | null = null;
 
   constructor(private analyzerService: AnalyzerService, public router: Router, private datePipe: DatePipe){}
 
   analyze():void{
     //-- Comienza el análisis.
     this.isAnalyzing = true;
+    this.errorMessage = null;
 
-    this.analyzerService.postAnalysis(this.url).subscribe((data: PostAnalysisResponse)=>{
+    this.analyzerService.postAnalysis(this.url).pipe(
+      catchError(error => {
+        this.isAnalyzing = false;
+        this.errorMessage = 'La URL proporcionada no es válida. ¡Prueba con otra!😉';
+        return of(null); //-- Devuelve un observable nulo para finalizar la cadena.
+      })
+    ).subscribe((data: PostAnalysisResponse)=>{
       this.analyzerService.analysis = {...data};
 
       if (data.isNew == true) {
